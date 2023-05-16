faa2bf2d27be89f4fc803ba2c5668816c6f669b7
Samuel Siau <samuelsiau@gmail.com>
2023-05-16 14:09:10 +1200

    Update config again!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  root: './shaggy-spiral',
-  site: 'https://samuelsiau.github.io/shaggy-spiral/'
-});
+export default defineConfig({});
