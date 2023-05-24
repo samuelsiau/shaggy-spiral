bd38fa285669a182126b8a7eaed3fa96e09ea47a
Samuel Siau <samuelsiau@gmail.com>
2023-05-24 13:38:48 +1200

    Updated

--- src/scripts/scrollingtrigger3.js
+++ src/scripts/scrollingtrigger3.js
@@ -7,15 +7,25 @@ gsap.registerPlugin(ScrollTrigger);
 // "play", "pause", "resume", "reset", "restart", "complete", "reverse", and "none".
 
 gsap.utils.toArray(".gsReveal").forEach(function(elem){
-  const $section = $(elem);
+  const $section = $(elem).find('.gsAni');
+
+  gsap.set($section, {
+    transformOrigin: "50% 100% 0",
+    transformPerspective: 400,
+    transformStyle: "preserve-3d"
+  })
+
   const animationChange = gsap.fromTo($section, {
+    rotateX: -27,
+    y: 20,
     autoAlpha: 0,
-    y: 20
   }, {
-    autoAlpha: 1,
+    rotateX: 0,
     y: 0,
-    duration: 1,
-    stagger: 0.7
+    autoAlpha: 1,
+    duration: 0.4,
+    stagger: 0.3,
+    ease: "back.out(1.4)",
   });
 
   ScrollTrigger.create({
@@ -23,7 +33,7 @@ gsap.utils.toArray(".gsReveal").forEach(function(elem){
     animation: animationChange,
     markers: true,
     toggleActions: "play reverse play reverse",
-    start: "center 80%",
-    end: "center 20%",
+    start: "center 90%",
+    end: "center 10%",
   })
 })
